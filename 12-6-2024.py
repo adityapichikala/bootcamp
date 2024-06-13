c8452faab16bd87e6a43a69048b2b4a6a0542a41
adityapichikala <adityapichikala@gmail.com>
2024-06-13 15:42:20 +0530

    add these

--- 12-6-2024.py
+++ 12-6-2024.py
@@ -38,7 +38,25 @@ but.pack(pady=20)
 root.mainloop()
 
 '''
+'''
+root=Tk()
 
+root.title("events and keyboard")
+
+root.geometry("400x400")
+
+def qwerty(event):
+    label=Label(root,text="hii how are you ")
+    label.pack()
+
+but=Button(root,text="click here")
+but.bind("<Enter>",qwerty)
+but.pack(pady=20)
+
+root.mainloop()
+'''
+
+'''
 root=Tk()
 
 root.title("events and keyboard")
@@ -52,5 +70,52 @@ def qwerty(event):
 but=Button(root,text="click here")
 but.bind("<Return>",qwerty)
 but.pack(pady=20)
+
+root.mainloop()
+'''
+
+
+'''
+root = Tk()
+
+C = Canvas(root, bg="yellow",
+		height=250, width=300)
+
+line = C.create_line(108, 120,
+					320, 40,
+					fill="green")
+
+arc = C.create_arc(180, 150, 80,
+				210, start=0,
+				extent=220,
+				fill="red")
+
+oval = C.create_oval(80, 30, 140,
+					150,
+					fill="blue")
+
+C.pack()
+mainloop()
+'''
+
+'''
+root = Tk()
+canvas = Canvas(root, width=400, height=300, bg="blue")
+canvas.pack()
+root.mainloop()
+'''
+
+root=Tk()
+root.title("checkbox")
+root.geometry("400x400")
+
+def show():
+    w=Label(root,text=a.get()).pack() 
+
+a=IntVar()
+c=Checkbutton(root,text="male",variable=a)
+c.pack()
+
+but=Button(root,text="show",command=show).pack()
 
 root.mainloop()
