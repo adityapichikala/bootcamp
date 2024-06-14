53bf1808b5fba18bf0e8377aa9e91063bd072862
adityapichikala <adityapichikala@gmail.com>
2024-06-14 22:36:40 +0530

    add these

--- 13-06-2024.py
+++ 13-06-2024.py
@@ -5,14 +5,27 @@
 #for connecting mysql and python or any type of language with mysql we need connector 
 
 import mysql.connector
-data=mysql.connector.connect(host="localhost",user="root",password="171119@aditya")
+
+data=mysql.connector.connect(host="localhost",user="root",password="171119@aditya",database="demo")
+
 print(data)
 
 mycursor=data.cursor()
 
 #mycursor.execute("create database demo")
 
+'''
 mycursor.execute("show databases")
 
 for x in mycursor:
     print(x)
+
+'''
+
+#mycursor.execute("create table hello(helloid int primary key auto_increment,helloname varchar(40) not null,helloph int not null)")
+sql="insert into hello(helloname,helloph)values(%s,%i)"
+values=("aditya",9494699433)
+
+#mycursor.execute("drop table hello")
+
+mycursor.execute(sql,values)
