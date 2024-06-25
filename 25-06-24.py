2cd720628eaa1207f67241797d5c48be84c505c4
adityapichikala <adityapichikala@gmail.com>
2024-06-25 18:15:55 +0530

    Add

---
+++ 25-06-24.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+'''
+NumPy, short for Numerical Python, is a library consisting of multidimensional array objects and a collection of routines for processing those arrays. It provides high-performance vector, matrix, and higher-dimensional data structures for Python. Some of the key features of NumPy include:
+
+Arrays: NumPy's main object is the homogeneous multidimensional array. It is a table of elements (usually numbers), all of the same type, indexed by a tuple of positive integers.
+
+Mathematical Functions: NumPy provides a large set of mathematical functions that can be performed on arrays. These include arithmetic operations, trigonometric functions, statistical functions, and more.
+
+Broadcasting: NumPy's broadcasting capability allows universal functions to deal with arrays of different shapes during arithmetic operations.
+
+Linear Algebra: NumPy provides a rich set of functions for linear algebra operations, such as matrix multiplication, matrix decomposition, eigenvalues, and more.
+
+Integration with Other Libraries: NumPy seamlessly integrates with other libraries in the scientific Python ecosystem, such as SciPy (scientific computing), Matplotlib (data visualization), and pandas (data manipulation).
+
+'''
+
+
+import numpy as np
+a=np.array([[1,2,3],[2,3,4]])
+print(a)
